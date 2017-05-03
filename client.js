a6348dde2e8c7bb55ea6ec5b694b71d9157a05a8
z106a <zz106a@gmail.com>
2017-05-03 21:02:25 +0300

    fetch added

--- client.js
+++ client.js
@@ -1,15 +1,25 @@
 const fileTailer = require('file-tail');
 const path = require('path');
 const fs = require('fs');
+const fetch = require('isomorphic-fetch');
 const sql = require('mssql');
+require('es6-promise').polyfill();
 
 const optionsBkFilePath = '/Program\ Files\ (x86)/BK/ServerBK/options.cfg';
 const logFilePath = '/Program\ Files\ (x86)/BK/ServerBK/Server.log';
+
+const BACKEND_BASE_URL = 'http://192.168.50.15:3000/api/';
+const HALLS = 'Sbks';
+
 const log = console.log.bind(console);
 
 const options = {};
 
-function init() {
+function getHalls() {
+	return fetch(BACKEND_BASE_URL+HALLS).then((resp) => resp.json()).catch(err => log(err));
+}
+
+function getOptions() {
 	optionFile = fs.readFileSync(optionsBkFilePath, [null, 'r']);
 	optionFile.toString().split("\n")
 		.map((line) => {
@@ -18,31 +28,51 @@ function init() {
 				options[parsed_line[0].trim()] = parsed_line[1].trim();
 			}
 		});
-	
+}
+
+function getClubId(dbconfig) {
+	return sql.connect(dbconfig).then(() => 
+		sql.query`select PropValue from dbo.CLUB where PropName = 'CLB_ID_GLOBAL'`
+		).then(result => {
+			sql.close();
+			return result.recordset[0].PropValue || undefined;
+		}).catch(err => {
+			log(err);
+			sql.close();
+		});
+}
+
+function init() {
+	let halls = getHalls();
+	halls.then((val) => log(val[0]));
+	getOptions();
+	getClubId({user: options["Database-Login"], password: options["Database-Password"],
+		server: "127.0.0.1", database: options["Database-Name"]})
+		.then(val => log(val));	
 }
 
 init();
 
-const dbconfig = {
-	user: options["Database-Login"],
-	password: options["Database-Password"],
-	server: "127.0.0.1",
-	database: options["Database-Name"]
-}
+log('after init');
+// const dbconfig = {
+// 	user: options["Database-Login"],
+// 	password: options["Database-Password"],
+// 	server: "127.0.0.1",
+// 	database: options["Database-Name"]
+// }
 
-var res = sql.connect(dbconfig).then(() => {
-	return sql.query`select PropValue from dbo.CLUB where PropName = 'CLB_ID_GLOBAL'`;
-}).then(result => {
-	// console.dir(result);
-	sql.close();
-	return result.recordset[0].PropValue || undefined;
-}).catch(err => {
-	log(err);
-	sql.close();
-});
+// const res = sql.connect(dbconfig).then(() => {
+// 	return sql.query`select PropValue from dbo.CLUB where PropName = 'CLB_ID_GLOBAL'`;
+// }).then(result => {
+// 	sql.close();
+// 	return result.recordset[0].PropValue || undefined;
+// }).catch(err => {
+// 	log(err);
+// 	sql.close();
+// });
 
-res.then((s) => log(s));
-sql.on('error', err => log(err));
+// res.then((s) => log(s));
+// sql.on('error', err => log(err));
 
 // ft = fileTailer.startTailing(logFilePath);
 // ft.on('line', (line) => log(line));
--- main.js
+++ main.js
@@ -1,53 +1,15 @@
-const os = require('os');
-const fs = require('fs');
-const path = require('path');
-const zlib = require('zlib');
-Tail = require('tail').Tail;
+var pcap = require("pcap"), 
+    pcap_session = pcap.createSession("", "tcp"),
+    matcher = /safari/i;
 
-const logFile = '/Program\ Files\ (x86)/BK/ServerBK/Server.log';
-const log = console.log.bind(console);
+console.log("Listening on " + pcap_session.device_name);
 
-let buf = new Buffer(65535);
-buf.fill(0);
-
-fs.open(logFile, 'r', (err, fd) => { 
-	// err && console.log(err);
-	read(fd, 0);
-})
-
-function read(fd, start_from){
-	fs.read(fd, buf, 0, buf.length, start_from, readFromFile);
-	fs.close(fd);
-}
-
-function readFromFile(err, bytesRead, buf){
-	console.log(buf.toString());
-}
-
-function logWatcher() {
-
-	fs.createReadStream(logFile, {start: 1024*1024, autoclose: true, flags: 'r'})
-	.on('data', (data) => log(data.length));
-}
-
-
-// fs.watchFile(logFile, (curr, prev) => log(curr, prev));
-
-// stream.on('data', (data) => log(stream.bytesRead));
-
-// setInterval(logWatcher.bind(), 1000);
-
-// fs.open(logFile, 'r', function(err, fd){
-
-// })
-// tail = new Tail(logFile);
-// tail.on("line", (data)=>log(data) );
-
-// readStream.on("data", function(chunk){
-//      console.log('chunk len in ts: '+chunk.length);
-//      zlib.deflate(chunk, (err, buffer) => {
-//          if (!err) {
-//              console.log(buffer.toString('base64').length); 
-//          }
-//      });
-// });
+pcap_session.on('packet', function (raw_packet) {
+    var packet = pcap.decode.packet(raw_packet),
+        data = packet.link.ip.tcp.data;
+    
+    if (data && matcher.test(data.toString())) {
+        console.log(pcap.print.packet(packet));
+        console.log(data.toString());
+    }
+});
