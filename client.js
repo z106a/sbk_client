9d1f8e449ff26dac0b873c27a9bebc9bf5b75359
z106a <zz106a@gmail.com>
2017-05-05 20:05:44 +0300

    ping added

--- client.js
+++ client.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 const fetch = require('isomorphic-fetch');
 const axios = require('axios');
 const sql = require('mssql');
+const exec = require('child_process').exec;
 require('es6-promise').polyfill();
 
 const optionsBkFilePath = '/Program\ Files\ (x86)/BK/ServerBK/options.cfg';
@@ -12,6 +13,7 @@ const logFilePath = '/Program\ Files\ (x86)/BK/ServerBK/Server.log';
 const BACKEND_BASE_URL = 'http://192.168.50.15:3000/api/';
 const HALLS_URL = 'Sbks';
 const EvTYPE_URL = 'EvTypes';
+const NetworkTests_URL = 'NetworkTests';
 
 const log = console.log.bind(console);
 
@@ -44,6 +46,13 @@ function getAvailabeEvType(pool) {
 	.catch(err => log(err));
 }
 
+function getPCUrl(pool) {
+	ip_match_re = /\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b/;
+	return pool.query`select PropValue from dbo.CLUB where PropName = 'PROCESSING_CENTER_API_URL'`
+	.then(res => res.recordset[0].PropValue.match(ip_match_re)[0] || undefined)
+	.catch(err => log(err));
+}
+
 async function getDbDataOnInit() {
 	config = {user: options["Database-Login"], password: options["Database-Password"],
 			server: "127.0.0.1", database: options["Database-Name"]};
@@ -51,7 +60,12 @@ async function getDbDataOnInit() {
 		let pool = await sql.connect(config);
 		let clubId = await getCLubID(pool);
 		let evTypes = await getAvailabeEvType(pool);
-		return {pool: pool, clubId: clubId, evTypes: evTypes};
+		let PCUrl = await getPCUrl(pool);
+		return {
+			pool: pool, clubId: clubId, evTypes: evTypes, PCUrl: PCUrl,
+			EvSrcAddrFlight: options["EventSourceAddressFlight"],
+			EvSrcAddrWeather: options["EventSourceAddressWeather"]
+		};
 	} catch (e) {
 		log(e);
 	}
@@ -68,22 +82,38 @@ async function init() {
 		log(e);
 	}
 }
+
 //--------------------------------
-var model_id = undefined;
+let model_id = undefined;
 init().then(async (init_data) => {
-	log(init_data);
 	let model = await findModelInDb(init_data.clubId); // here we get field 'id'
 	if (model) {
-		model_id = model.id
-		log(model.id);
+		model_id = model.id // set global model_id
 		await patchData(model.id, {'enabled_events_type': init_data.evTypes});
 		await getLastEvTypeStatus(init_data.pool);
-		ft = fileTailer.startTailing(logFilePath);
-		ft.on('line', async (line) => (await patchData(model.id, {'bk_server_log': line})) );
+		init_data.PCUrl && interval(init_data.PCUrl, 30000);
+		init_data.EvSrcAddrWeather && interval(init_data.EvSrcAddrWeather, 30000);
+		init_data.EvSrcAddrFlight && interval(init_data.EvSrcAddrFlight, 30000);
+
+		// 
+		// ft = fileTailer.startTailing(logFilePath);
+		// ft.on('line', async (line) => (await patchData(model.id, {'bk_server_log': line})) );
 	}
 	init_data.pool.close();
 });
 //--------------------------------
+function interval(ip, tick){
+	return setInterval(pingSomewhat.bind(this, ip), tick)
+}
+
+function pingSomewhat(ip) {
+	log(String(ip));
+	exec(`chcp 65001 |ping ${ip}`, (error, stdout, stderr) => {
+		putNetworkTests({ip: ip.replace(/\./g, '_'),
+		 result: stdout.replace(/[\n\r]+/g, '\n') || stderr.replace(/[\n\r]+/g, '\n') || error.replace(/[\n\r]+/g, '\n')});		
+	});
+}
+
 function findModelInDb(id_hall) {
 	return fetch(`${BACKEND_BASE_URL}${HALLS_URL}/findOne?filter[where][id_hall]=${id_hall}`)
 		.then(resp => resp.json()).then(model => {
@@ -92,7 +122,8 @@ function findModelInDb(id_hall) {
 }
 
 function patchData(id, val) {
-	let header = 'Content-Type: application/json'
+	let header = 'Content-Type: application/json';
+	log(val);
 	return axios.patch(`${BACKEND_BASE_URL}${HALLS_URL}/${id}`, 
 		val)
 		.then(() => log('patchSuccess'))
@@ -110,5 +141,10 @@ function getLastEvTypeStatus(pool) {
 function putEvTypeStatus(data) {
 	data.sbk_fk = model_id;
 	axios.put(`${BACKEND_BASE_URL}${EvTYPE_URL}`, data).catch(e => log(e));
+}
+
+function putNetworkTests(data) {
+	data.sbk_fk = model_id;
+	axios.patch(`${BACKEND_BASE_URL}${NetworkTests_URL}`, data).catch(e => log(e));
 }
 
